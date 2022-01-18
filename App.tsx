efd63ddf82227d1ce0b655cfcea1d11b792d5ae7
Zdeněk Odehnal <odehnal.business@gmail.com>
2022-01-18 17:37:53 +0100

    finish projecT

--- App.tsx
+++ App.tsx
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
 
 export default function App() {
+  const [DiceValue, setDiceValue] = useState(0);
+  const onPress = () => setDiceValue(RollDice);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Image
+        style={styles.diceIcon}
+        source={require('./assets/dice.png')}
+      />
+      <Text style={styles.outputContainer}>
+        Kostka byla hozena a padlo: {DiceValue}
+      </Text>
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text>Hodit kostku</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+function RollDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+  },
+  outputContainer: {
+    alignItems: "center",
+    padding: 10,
+    fontSize: 20,
+  },
+  diceIcon: {
+    width: 50,
+    height: 50,
   },
 });
